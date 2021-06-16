3ff18a54fb98e1ee8380d300b64e89073f803962
gilangap01 <harzthdev01@gmail.com>
2021-06-16 21:42:04 +0700

    Mengubah simbol kali/bagi dan mengganti font-family

--- script.js
+++ script.js
@@ -4,18 +4,27 @@ const input = document.querySelector('.input');
 const output = document.querySelector('.output');
 
 let out = '';
-let cekAngka = true, cekOperator = false;
+let inp = '';
 
 input.addEventListener('click', function(e){
 	if(e.target.className == 'angka'){
 		out += e.target.textContent;
-		cekOperator = true;
+		inp += e.target.textContent;
 	} else if(e.target.textContent == 'C'){
 		out = '';
-	} else if(e.target.className == 'operator' && cekOperator == true){
-		out += e.target.textContent;
+	} else if(e.target.className == 'operator'){
+		if(e.target.textContent == '÷'){
+			out += '/';
+			inp += e.target.textContent; 
+		} else if (e.target.textContent == 'x'){
+			inp += '*';
+			out += e.target.textContent; 
+		} else {
+			out += e.target.textContent;
+			inp += e.target.textContent;
+		}
 	} else if(e.target.className == 'hasil'){
-		let hasil = eval(out);
+		let hasil = eval(inp);
 		out = hasil;
 	}
 	
