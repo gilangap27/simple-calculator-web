6515b5c9be7daaa0921eec7c90e2a9d246a0dc79
gilangap01 <harzthdev01@gmail.com>
2021-06-16 23:08:30 +0700

    Memperbaiki beberapa bug

--- js/script.js
+++ js/script.js
@@ -1,34 +1,43 @@
-
-
+//Membuat DOM Selector untuk Element HTML
 const input = document.querySelector('.input');
 const output = document.querySelector('.output');
 
+//Variable
 let out = '';
-let inp = '';
 
-input.addEventListener('click', function(e){
-	if(e.target.className == 'angka'){
-		out += e.target.textContent;
-		inp += e.target.textContent;
-	} else if(e.target.textContent == 'C'){
-		out = '';
-	} else if(e.target.textContent == 'DEL'){
-		out = out.slice(0, -1);
-	} else if(e.target.className == 'operator'){
-		if(e.target.textContent == '÷'){
-			inp += '/';
-			out += e.target.textContent; 
-		} else if (e.target.textContent == 'x'){
-			inp += '*';
-			out += e.target.textContent; 
-		} else {
-			out += e.target.textContent;
-			inp += e.target.textContent;
-		}
-	} else if(e.target.className == 'hasil'){
-		let hasil = eval(inp);
-		out = hasil;
-	}
-	
-	output.textContent = out;
+//Memberi Event
+input.addEventListener('click', function(e) {
+
+    if (e.target.className == 'angka') {
+        out += e.target.textContent;
+
+    } else if (e.target.textContent == 'C') {
+        out = '';
+
+    } else if (e.target.textContent == 'DEL') {
+        out = out.slice(0, -1);
+
+    } else if (e.target.className == 'operator') {
+        const cek = /\d/;
+        let pot = out.substr(out.length - 1);
+
+        if (cek.test(pot)) {
+            out += e.target.textContent;
+
+        } else {
+            let temp = out.split('');
+            temp.pop();
+            temp.push(e.target.textContent);
+            let hasil = temp.join('');
+            out = hasil;
+
+        }
+
+    } else if (e.target.className == 'hasil') {
+        let hasil = eval(out);
+        out = hasil;
+
+    }
+
+    output.textContent = out;
 })
