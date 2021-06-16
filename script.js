535762276322e108de71b0c5fe5854ac83fcc4f9
gilangap01 <harzthdev01@gmail.com>
2021-06-16 21:22:27 +0700

    Membuat kalkulator dapat bekerja

--- script.js
+++ script.js
@@ -3,45 +3,21 @@
 const input = document.querySelector('.input');
 const output = document.querySelector('.output');
 
-let out = '', angka = '';
-let bil1 = [];
-let bil2 = [];
-let operator = [];
-let betul = true;
+let out = '';
+let cekAngka = true, cekOperator = false;
 
 input.addEventListener('click', function(e){
 	if(e.target.className == 'angka'){
 		out += e.target.textContent;
-		angka += e.target.textContent
+		cekOperator = true;
 	} else if(e.target.textContent == 'C'){
 		out = '';
-	} 
-	else if(e.target.className == 'operator'){
-
-		out += ' '+e.target.textContent+' ';
-
-		if(betul == true){
-			bil1.push(Number(angka));
-			angka = 0;
-			betul = false;
-		} else if(betul == false){
-			bil2.push(Number(angka));
-			angka = 0;
-			betul = true;
-		}
-
-		if(e.target.textContent == ':'){
-			operator.push(':');
-		} else if(e.target.textContent == 'x'){
-			operator.push('x');
-		} else if(e.target.textContent == '-'){
-			operator.push('-');
-		} else if(e.target.textContent == '+'){
-			operator.push('+');
-		} else if(e.target.textContent == '='){
-			
-			out = hasil;
-		} 
-	} 
+	} else if(e.target.className == 'operator' && cekOperator == true){
+		out += e.target.textContent;
+	} else if(e.target.className == 'hasil'){
+		let hasil = eval(out);
+		out = hasil;
+	}
+	
 	output.textContent = out;
 })
